d0989372e0d74fbfe3fcb5da779d8e95c38faedf
Joe <pulkjr@gmail.com>
2016-06-27 23:00:57 -0400

    Initial Pull Command and Module Files
    Created the initial Pull Command and Module Configuration Files

---
+++ src/PoSHVersionControl.psm1
@@ -0,0 +1,19 @@
+﻿#0001,New-PsCommand,00.00.01,2016-06-17 20:00:00,Joseph Pulk$
+
+[xml]$config = Get-Content "$($PSScriptRoot)\Resources\config.xml"
+# --- Global Variables --- #
+
+# --- Module Variables --- #
+New-Variable -Name MasterRepoPath -Value $config.PoSHVersionControl.distro.path -Description "The path to the Repository" -Visibility Private -Scope Script
+New-Variable -Name MasterTemplatePath -Value $config.PoSHVersionControl.userPreference.defaultTemplate.name -Description "The path to the template file." -Visibility Private -Scope Script
+
+# --- Import Functions --- #
+try {
+    $manifest = Import-Csv "$($PSScriptRoot)\Resources\manifest.csv"
+    ForEach($file in $manifest.name){
+        . "$($PSScriptRoot)\Functions\$file"
+    }
+}
+catch {
+    throw
+}
