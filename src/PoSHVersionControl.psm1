5419f2150086abbae769fa78ebebd1e72682f816
Joe <pulkjr@gmail.com>
2016-08-10 22:51:51 -0400

    JSON Config and MasterRepoInfo Variable
    Modified config file to use json instead of xml. This allows the input
    to come in as a native powershell object instead of an xml object and
    will make it easier to work with in the future.

--- src/PoSHVersionControl.psm1
+++ src/PoSHVersionControl.psm1
@@ -1,19 +1,30 @@
 ﻿#0001,New-PsCommand,00.00.01,2016-06-17 20:00:00,Joseph Pulk$
 
-[xml]$config = Get-Content "$($PSScriptRoot)\Resources\config.xml"
 # --- Global Variables --- #
 
 # --- Module Variables --- #
-New-Variable -Name MasterRepoPath -Value $config.PoSHVersionControl.distro.path -Description "The path to the Repository" -Visibility Private -Scope Script
-New-Variable -Name MasterTemplatePath -Value $config.PoSHVersionControl.userPreference.defaultTemplate.name -Description "The path to the template file." -Visibility Private -Scope Script
+$parametrs = @{
+    'Name' = 'MasterRepoInfo';
+    'Value' = $(ConvertFrom-Json -InputObject (Get-Content -Path "$($PSScriptRoot)\Resources\config.json" -Raw));
+    'Description' = 'Array that holds all of the repository information';
+    #'Visibility' = 'Private';
+    #'Scope' = 'Script';
+
+}
+New-Variable @parametrs
+
+
 
 # --- Import Functions --- #
-try {
-    $manifest = Import-Csv "$($PSScriptRoot)\Resources\manifest.csv"
-    ForEach($file in $manifest.name){
+try
+{
+    $manifest = Import-Csv -Path "$($PSScriptRoot)\Resources\manifest.csv"
+    ForEach($file in $manifest.name)
+    {
         . "$($PSScriptRoot)\Functions\$file"
     }
 }
-catch {
+catch
+{
     throw
 }
